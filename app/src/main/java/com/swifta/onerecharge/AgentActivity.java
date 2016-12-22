3a9e19392f67f5864d1c8372cacf556e874dfd66
Moyinoluwa <moyinoluwaa@gmail.com>
2016-12-22 16:50:17 +0100

    feat: Implement CollapsingLayout on AgentActivity

--- app/src/main/java/com/swifta/onerecharge/AgentActivity.java
+++ app/src/main/java/com/swifta/onerecharge/AgentActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,7 +17,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.swifta.onerecharge.agentdashboard.AgentSummary;
 import com.swifta.onerecharge.agentdashboard.DashboardFragment;
 import com.swifta.onerecharge.agentquickrecharge.QuickRechargeFragment;
 import com.swifta.onerecharge.agentquicktransactionhistory.AgentQuickTransactionHistoryFragment;
@@ -24,6 +27,18 @@ import com.swifta.onerecharge.agentregistration.AgentRegistrationActivity;
 import com.swifta.onerecharge.agentscheduledrecharge.ScheduledRechargeFragment;
 import com.swifta.onerecharge.agentscheduledtransactionhistory.AgentScheduledTransactionHistoryFragment;
 import com.swifta.onerecharge.resetagentpassword.ProfileActivity;
+import com.swifta.onerecharge.util.AgentService;
+import com.swifta.onerecharge.util.Url;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class AgentActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -31,17 +46,30 @@ public class AgentActivity extends AppCompatActivity
     TextView agentHeaderEmailView;
     SharedPreferences sharedPreferences;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
+    @BindView(R.id.collapsingLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agent);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        sharedPreferences = getSharedPreferences(getString (R.string
+        ButterKnife.bind(this);
+
+        sharedPreferences = getSharedPreferences(getString(R.string
                 .agent_shared_preference_name), Context.MODE_PRIVATE);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+        setUpCollapsingToolbarWithCurrentValue();
+
+        getAgentSummary();
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -119,6 +147,188 @@ public class AgentActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void setUpCollapsingToolbarWithCurrentValue() {
+        collapsingToolbarLayout.setTitle(getResources().getString(R.string
+                .wallet_balance, getWalletBalance()));
+    }
+
+    private String getWalletBalance() {
+        return sharedPreferences.getString(getResources().getString(R.string
+                .saved_wallet_balance), "0.00");
+    }
+
+    private String getEmailAddress() {
+        return sharedPreferences.getString(getResources().getString(R.string
+                .saved_email_address), "");
+    }
+
+    private String getToken() {
+        return sharedPreferences.getString(getResources().getString(R.string
+                .saved_auth_token), "");
+    }
+
+    private void saveProfileRulesResult(String walletBalance, String
+            msisdnPurchaseLimit, String minimumWalletBalance, String
+                                                airtimePurchaseDiscount, String airtimeSoldCommission) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(getResources().getString(R.string
+                .saved_wallet_balance), walletBalance);
+        editor.putString(getResources().getString(R.string
+                .saved_msisdn_purchase_limit), msisdnPurchaseLimit);
+        editor.putString(getResources().getString(R.string
+                .saved_minimum_wallet_balance), minimumWalletBalance);
+        editor.putString(getResources().getString(R.string
+                .saved_airtime_purchase_discount), airtimePurchaseDiscount);
+        editor.putString(getResources().getString(R.string
+                .saved_airtime_sold_commission), airtimeSoldCommission);
+
+        editor.apply();
+    }
+
+    private void saveProfileSummaryResult(String referralId, String
+            agentType, String agentClass, String dateAndTime, int
+                                                  totalTransactions, int successfulTransactions, int
+                                                  totalAmountSold, int successfulTransactionAmount, String
+                                                  successfulTransactionTime,
+                                          int totalFailedTransactions, int
+                                                  totalFailedTransactionAmount, int lastFailedAmount, String lastFailedTime, String lastFailedDescription) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(getResources().getString(R.string.saved_referral_id)
+                , referralId);
+        editor.putString(getResources().getString(R.string.saved_agent_type)
+                , agentType);
+        editor.putString(getResources().getString(R.string.saved_agent_class)
+                , agentClass);
+        editor.putString(getResources().getString(R.string.saved_date_and_time)
+                , dateAndTime);
+        editor.putInt(getResources().getString(R.string.saved_total_transactions)
+                , totalTransactions);
+        editor.putInt(getResources().getString(R.string.saved_successful_transactions)
+                , successfulTransactions);
+        editor.putInt(getResources().getString(R.string.saved_total_amount_sold)
+                , totalAmountSold);
+        editor.putInt(getResources().getString(R.string.saved_last_successful_amount)
+                , successfulTransactionAmount);
+        editor.putString(getResources().getString(R.string.saved_last_successful_time)
+                , successfulTransactionTime);
+        editor.putInt(getResources().getString(R.string
+                .saved_total_failed_transaction), totalFailedTransactions);
+        editor.putInt(getResources().getString(R.string
+                .saved_total_failed_transaction_amount), totalFailedTransactionAmount);
+        editor.putInt(getResources().getString(R.string
+                .saved_last_failed_amount), lastFailedAmount);
+        editor.putString(getResources().getString(R.string.saved_last_failed_time)
+                , lastFailedTime);
+        editor.putString(getResources().getString(R.string.saved_failed_transaction_description)
+                , lastFailedDescription);
+
+        editor.apply();
+    }
+
+    private void setUpWalletWithCurrentValue(String walletBalance) {
+        collapsingToolbarLayout.setTitle(getResources().getString(R.string
+                .wallet_balance, walletBalance));
+    }
+
+    private void getAgentSummary() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Url.BASE_URL)
+                .build();
+
+        String emailAddress = getEmailAddress();
+        String authToken = getToken();
+
+        final AgentService agentService = retrofit
+                .create(AgentService.class);
+        final Observable<AgentSummary> agent = agentService
+                .getSummary(emailAddress, authToken);
+
+        agent.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<AgentSummary>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNext(AgentSummary agentSummary) {
+                        if (agentSummary.getStatus() == 1) {
+
+                            String walletBalance = agentSummary.getData
+                                    ().getWalletBalance();
+                            String msisdnPurchaseLimit = String.valueOf
+                                    (agentSummary.getData().getRulesLimits()
+                                            .getMsisdnPurchaseLimit());
+                            String minimumWalletBalance = String.valueOf
+                                    (agentSummary.getData().getRulesLimits()
+                                            .getMinimumWallet());
+                            String airtimePurchaseDiscount = String.valueOf
+                                    (agentSummary.getData().getRulesLimits()
+                                            .getAirtimePurchaseDiscount());
+                            String airtimeSoldCommission = String.valueOf
+                                    (agentSummary.getData().getRulesLimits()
+                                            .getAirtimeSoldCommission());
+
+                            String referralId = String.valueOf(agentSummary
+                                    .getData().getReferralId());
+
+                            String agentType = String.valueOf(agentSummary
+                                    .getData().getAgentType());
+                            String agentClass = String.valueOf(agentSummary
+                                    .getData().getAgentClass());
+                            String dateAndTimeCreated = String.valueOf
+                                    (agentSummary.getData().getJoined());
+                            int totalTransactions = agentSummary.getData().getTotal();
+                            int savedSuccessfulTransactions = agentSummary
+                                    .getData().getTotalSuccess();
+                            int totalAmountSold = agentSummary.getData()
+                                    .getAmountSuccess();
+                            int successfulTransactionAmount = agentSummary
+                                    .getData().getLastSuccessAmount();
+                            String successfulTransactionTime = agentSummary
+                                    .getData().getLastSuccessTime();
+                            int totalFailedTransactions = agentSummary
+                                    .getData().getTotalFailed();
+                            int totalFailedTransactionAmount = agentSummary
+                                    .getData().getAmountFailed();
+                            int lastFailedAmount = agentSummary
+                                    .getData().getLastFailedAmount();
+                            String lastFailedTime = agentSummary
+                                    .getData().getLastFailedTime();
+                            String lastFailedDescription = agentSummary
+                                    .getData().getLastFailedDescription();
+
+                            setUpWalletWithCurrentValue(walletBalance);
+
+                            saveProfileRulesResult(walletBalance,
+                                    msisdnPurchaseLimit,
+                                    minimumWalletBalance,
+                                    airtimePurchaseDiscount, airtimeSoldCommission);
+
+                            saveProfileSummaryResult(referralId, agentType,
+                                    agentClass, dateAndTimeCreated,
+                                    totalTransactions,
+                                    savedSuccessfulTransactions,
+                                    totalAmountSold,
+                                    successfulTransactionAmount,
+                                    successfulTransactionTime,
+                                    totalFailedTransactions,
+                                    totalFailedTransactionAmount,
+                                    lastFailedAmount, lastFailedTime, lastFailedDescription);
+                        }
+                    }
+                });
     }
 
     private void displayFragment(Fragment fragment) {
--- app/src/main/java/com/swifta/onerecharge/agentdashboard/DashboardFragment.java
+++ app/src/main/java/com/swifta/onerecharge/agentdashboard/DashboardFragment.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.swifta.onerecharge.R;
@@ -35,11 +34,7 @@ import rx.schedulers.Schedulers;
  * A simple {@link Fragment} subclass.
  */
 public class DashboardFragment extends Fragment {
-    private View view;
     private SharedPreferences sharedPreferences;
-
-    @BindView(R.id.wallet_balance_text)
-    TextView walletBalanceText;
 
     @BindView(R.id.tabLayout)
     TabLayout tabLayout;
@@ -55,7 +50,7 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_dashboard, container,
+        View view = inflater.inflate(R.layout.fragment_dashboard, container,
                 false);
 
         ButterKnife.bind(this, view);
@@ -71,9 +66,7 @@ public class DashboardFragment extends Fragment {
         initTabInstances();
         tabLayout.setupWithViewPager(viewPager);
 
-        setUpUiWithDefaultValues();
-
-        getAgentSummary();
+        setupViewPager(viewPager, getChildFragmentManager());
 
         getAgentApiKey();
 
@@ -113,180 +106,6 @@ public class DashboardFragment extends Fragment {
     private String getToken() {
         return sharedPreferences.getString(getResources().getString(R.string
                 .saved_auth_token), "");
-    }
-
-    private String getWalletBalance() {
-        return sharedPreferences.getString(getResources().getString(R.string
-                .saved_wallet_balance), "0.00");
-    }
-
-    private void setUpUiWithDefaultValues() {
-        walletBalanceText.setText(getResources().getString(R.string
-                .wallet_balance, getWalletBalance()));
-    }
-
-    private void saveProfileRulesResult(String walletBalance, String
-            msisdnPurchaseLimit, String minimumWalletBalance, String
-                                                airtimePurchaseDiscount, String airtimeSoldCommission) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(getResources().getString(R.string
-                .saved_wallet_balance), walletBalance);
-        editor.putString(getResources().getString(R.string
-                .saved_msisdn_purchase_limit), msisdnPurchaseLimit);
-        editor.putString(getResources().getString(R.string
-                .saved_minimum_wallet_balance), minimumWalletBalance);
-        editor.putString(getResources().getString(R.string
-                .saved_airtime_purchase_discount), airtimePurchaseDiscount);
-        editor.putString(getResources().getString(R.string
-                .saved_airtime_sold_commission), airtimeSoldCommission);
-
-        editor.apply();
-    }
-
-    private void saveProfileSummaryResult(String referralId, String
-            agentType, String agentClass, String dateAndTime, int
-                                                  totalTransactions, int successfulTransactions, int
-                                                  totalAmountSold, int successfulTransactionAmount, String
-                                                  successfulTransactionTime,
-                                          int totalFailedTransactions, int
-                                                  totalFailedTransactionAmount, int lastFailedAmount, String lastFailedTime, String lastFailedDescription) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(getResources().getString(R.string.saved_referral_id)
-                , referralId);
-        editor.putString(getResources().getString(R.string.saved_agent_type)
-                , agentType);
-        editor.putString(getResources().getString(R.string.saved_agent_class)
-                , agentClass);
-        editor.putString(getResources().getString(R.string.saved_date_and_time)
-                , dateAndTime);
-        editor.putInt(getResources().getString(R.string.saved_total_transactions)
-                , totalTransactions);
-        editor.putInt(getResources().getString(R.string.saved_successful_transactions)
-                , successfulTransactions);
-        editor.putInt(getResources().getString(R.string.saved_total_amount_sold)
-                , totalAmountSold);
-        editor.putInt(getResources().getString(R.string.saved_last_successful_amount)
-                , successfulTransactionAmount);
-        editor.putString(getResources().getString(R.string.saved_last_successful_time)
-                , successfulTransactionTime);
-        editor.putInt(getResources().getString(R.string
-                .saved_total_failed_transaction), totalFailedTransactions);
-        editor.putInt(getResources().getString(R.string
-                .saved_total_failed_transaction_amount), totalFailedTransactionAmount);
-        editor.putInt(getResources().getString(R.string
-                .saved_last_failed_amount), lastFailedAmount);
-        editor.putString(getResources().getString(R.string.saved_last_failed_time)
-                , lastFailedTime);
-        editor.putString(getResources().getString(R.string.saved_failed_transaction_description)
-                , lastFailedDescription);
-
-        editor.apply();
-    }
-
-    private void setUpWalletWithCurrentValue(String walletBalance) {
-        walletBalanceText.setText(getResources().getString(R.string
-                .wallet_balance, walletBalance));
-    }
-
-    private void getAgentSummary() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(Url.BASE_URL)
-                .build();
-
-        String emailAddress = getEmailAddress();
-        String authToken = getToken();
-
-        final AgentService agentService = retrofit
-                .create(AgentService.class);
-        final Observable<AgentSummary> agent = agentService
-                .getSummary(emailAddress, authToken);
-
-        agent.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .unsubscribeOn(Schedulers.io())
-                .subscribe(new Subscriber<AgentSummary>() {
-                    @Override
-                    public void onCompleted() {
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText(getActivity(), e.getLocalizedMessage(),
-                                Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onNext(AgentSummary agentSummary) {
-                        if (agentSummary.getStatus() == 1) {
-
-                            String walletBalance = agentSummary.getData
-                                    ().getWalletBalance();
-                            String msisdnPurchaseLimit = String.valueOf
-                                    (agentSummary.getData().getRulesLimits()
-                                            .getMsisdnPurchaseLimit());
-                            String minimumWalletBalance = String.valueOf
-                                    (agentSummary.getData().getRulesLimits()
-                                            .getMinimumWallet());
-                            String airtimePurchaseDiscount = String.valueOf
-                                    (agentSummary.getData().getRulesLimits()
-                                            .getAirtimePurchaseDiscount());
-                            String airtimeSoldCommission = String.valueOf
-                                    (agentSummary.getData().getRulesLimits()
-                                            .getAirtimeSoldCommission());
-
-                            String referralId = String.valueOf(agentSummary
-                                    .getData().getReferralId());
-
-                            String agentType = String.valueOf(agentSummary
-                                    .getData().getAgentType());
-                            String agentClass = String.valueOf(agentSummary
-                                    .getData().getAgentClass());
-                            String dateAndTimeCreated = String.valueOf
-                                    (agentSummary.getData().getJoined());
-                            int totalTransactions = agentSummary.getData().getTotal();
-                            int savedSuccessfulTransactions = agentSummary
-                                    .getData().getTotalSuccess();
-                            int totalAmountSold = agentSummary.getData()
-                                    .getAmountSuccess();
-                            int successfulTransactionAmount = agentSummary
-                                    .getData().getLastSuccessAmount();
-                            String successfulTransactionTime = agentSummary
-                                    .getData().getLastSuccessTime();
-                            int totalFailedTransactions = agentSummary
-                                    .getData().getTotalFailed();
-                            int totalFailedTransactionAmount = agentSummary
-                                    .getData().getAmountFailed();
-                            int lastFailedAmount = agentSummary
-                                    .getData().getLastFailedAmount();
-                            String lastFailedTime = agentSummary
-                                    .getData().getLastFailedTime();
-                            String lastFailedDescription = agentSummary
-                                    .getData().getLastFailedDescription();
-
-                            setUpWalletWithCurrentValue(walletBalance);
-
-                            saveProfileRulesResult(walletBalance,
-                                    msisdnPurchaseLimit,
-                                    minimumWalletBalance,
-                                    airtimePurchaseDiscount, airtimeSoldCommission);
-
-                            saveProfileSummaryResult(referralId, agentType,
-                                    agentClass, dateAndTimeCreated,
-                                    totalTransactions,
-                                    savedSuccessfulTransactions,
-                                    totalAmountSold,
-                                    successfulTransactionAmount,
-                                    successfulTransactionTime,
-                                    totalFailedTransactions,
-                                    totalFailedTransactionAmount,
-                                    lastFailedAmount, lastFailedTime, lastFailedDescription);
-
-                            setupViewPager(viewPager, getChildFragmentManager());
-                        }
-                    }
-                });
     }
 
     private void getNetworkList() {
