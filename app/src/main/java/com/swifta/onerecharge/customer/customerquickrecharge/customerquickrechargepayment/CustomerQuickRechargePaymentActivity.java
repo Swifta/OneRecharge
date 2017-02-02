71c1a7820e2df1c498dd070ad5f9f0c569a37c17
Moyinoluwa <moyinoluwaa@gmail.com>
2017-02-02 13:41:22 +0100

    feat: Implement quick recharge from customer's card

--- app/src/main/java/com/swifta/onerecharge/customer/customerquickrecharge/customerquickrechargepayment/CustomerQuickRechargePaymentActivity.java
+++ app/src/main/java/com/swifta/onerecharge/customer/customerquickrecharge/customerquickrechargepayment/CustomerQuickRechargePaymentActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,11 +13,23 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.swifta.onerecharge.R;
+import com.swifta.onerecharge.agent.agentquickrecharge.RechargeResponseFragment;
+import com.swifta.onerecharge.customer.customerquickrecharge.customerquickrechargerequestmodel.CustomerQuickRechargeRequest;
+import com.swifta.onerecharge.customer.customerquickrecharge.customerquickrechargeresponsemodel.CustomerQuickRechargeResponse;
+import com.swifta.onerecharge.util.CustomerService;
 import com.swifta.onerecharge.util.InternetConnectivity;
+import com.swifta.onerecharge.util.Url;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class CustomerQuickRechargePaymentActivity extends AppCompatActivity {
 
@@ -47,10 +60,19 @@ public class CustomerQuickRechargePaymentActivity extends AppCompatActivity {
     @BindView(R.id.card_payment_container)
     LinearLayout cardPaymentContainer;
 
+    RechargeResponseFragment successfulFragment;
+
     String phoneNumber, country, networkProvider, email;
     int amount;
     String cardNumber, monthValue, yearValue, cvv, cardPin;
+
     static final boolean IS_CARD_TRANSACTION = true;
+    private static final int TRANSACTION_FAILED = 0;
+    private static final int TRANSACTION_SUCCESSFUL = 1;
+    private static final String TRANSACTION_SUCCESSFUL_MESSAGE = "Transaction request sent " +
+            "successfully!";
+    private static final String TRANSACTION_ERROR_MESSAGE = "Transaction " +
+            "unsuccessful. Please try again.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -183,5 +205,68 @@ public class CustomerQuickRechargePaymentActivity extends AppCompatActivity {
         cardPaymentContainer.setVisibility(View.GONE);
         progressBar.setVisibility(View.VISIBLE);
 
+    }
+
+    private void performQuickRecharge() {
+
+        CustomerQuickRechargeRequest walletQuickRechargeRequest = new
+                CustomerQuickRechargeRequest(phoneNumber, amount, networkProvider,
+                IS_CARD_TRANSACTION, email);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Url.BASE_URL)
+                .build();
+
+        CustomerService customerService = retrofit.create(CustomerService.class);
+        final Observable<CustomerQuickRechargeResponse> quickRecharge =
+                customerService.performCustomerQuickRechargeFromWallet
+                        ("sMHuflOVZYEuCvXW1SGWmIMoj0aV5q1D", "oZFLQVYpRO9Ur8i6H8Q1J5c3RMgt0fb0",
+                                walletQuickRechargeRequest);
+
+        quickRecharge.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(new Subscriber<CustomerQuickRechargeResponse>() {
+
+                    @Override
+                    public void onCompleted() {
+                        progressBar.setVisibility(View.GONE);
+                        cardPaymentContainer.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        showResultDialog(TRANSACTION_ERROR_MESSAGE, TRANSACTION_FAILED);
+                    }
+
+                    @Override
+                    public void onNext(CustomerQuickRechargeResponse quickRechargeResponse) {
+
+                        if (quickRechargeResponse.getStatus() == 1) {
+                            showResultDialog(TRANSACTION_SUCCESSFUL_MESSAGE, TRANSACTION_SUCCESSFUL);
+                            clearInputFields();
+                            finish();
+                        } else if (quickRechargeResponse.getStatus() == 0) {
+                            showResultDialog(quickRechargeResponse.getData().getMessage(),
+                                    TRANSACTION_FAILED);
+                        }
+                    }
+                });
+    }
+
+    private void showResultDialog(String message, int status) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        successfulFragment = RechargeResponseFragment.newInstance(message, status);
+        successfulFragment.show(fragmentManager, "");
+    }
+
+    private void clearInputFields() {
+        cardNumberText.setText("");
+        expiryDateMonthText.setText("");
+        expiryDateYearText.setText("");
+        cvvText.setText("");
+        cardPinText.setText("");
     }
 }
