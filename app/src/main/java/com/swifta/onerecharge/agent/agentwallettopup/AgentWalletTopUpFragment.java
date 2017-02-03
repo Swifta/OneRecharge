5dc8f6d339466aa125f45c7c9881830285d2df36
Moyinoluwa <moyinoluwaa@gmail.com>
2017-02-03 16:48:45 +0100

    feat: Clear menu in AgentWalletTopUpFragment

--- app/src/main/java/com/swifta/onerecharge/agent/agentwallettopup/AgentWalletTopUpFragment.java
+++ app/src/main/java/com/swifta/onerecharge/agent/agentwallettopup/AgentWalletTopUpFragment.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -69,6 +72,12 @@ public class AgentWalletTopUpFragment extends Fragment {
 
     public AgentWalletTopUpFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -164,6 +173,12 @@ public class AgentWalletTopUpFragment extends Fragment {
         paymentActivityIntent.putExtra("country", countryChoiceSpinner.getSelectedItem().toString
                 ());
         startActivity(paymentActivityIntent);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
     }
 
     private String getEmailAddress() {
