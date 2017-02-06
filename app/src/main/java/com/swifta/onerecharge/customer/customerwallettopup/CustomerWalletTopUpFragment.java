46061e3eea37be776da6649b19d9e887fe0dd831
Moyinoluwa <moyinoluwaa@gmail.com>
2017-02-06 10:48:40 +0100

    feat: Remove menu in customer wallet top up fragment

--- app/src/main/java/com/swifta/onerecharge/customer/customerwallettopup/CustomerWalletTopUpFragment.java
+++ app/src/main/java/com/swifta/onerecharge/customer/customerwallettopup/CustomerWalletTopUpFragment.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -64,6 +67,12 @@ public class CustomerWalletTopUpFragment extends Fragment {
 
     public CustomerWalletTopUpFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -138,6 +147,12 @@ public class CustomerWalletTopUpFragment extends Fragment {
                 switchToPaymentActivity();
             }
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
     }
 
     private void switchToPaymentActivity() {
